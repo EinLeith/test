60731611309279ccf04a9ad0f0a530cf5929c3db
leith.al-shalbi <leith.alshalbi@analytic-company.com>
2025-02-17 17:32:37 +0100

    added some stats

--- LeithTest.java
+++ LeithTest.java
@@ -10,28 +10,31 @@ public class LeithTest {
         Random rand = new Random();
         String input = sc.nextLine();
 
-        Pattern Mercedes = Pattern.compile("(Mercedes|Benz)\\.{6}");
-        Matcher mercedes = Mercedes.matcher(input);
 
-        if (mercedes.find()) {
-            System.out.println(System.out.println(mercedes.group());import java.util.Scanner;
-            import java.util.regex.*;
-            import java.util.Random;
+        String[] Team = {"Manchester United", "Manchester City",
+                "Liverpool", "Chelsea", "Arsenal", "Tottenham",
+                "Bayern Munich", "Borussia Dortmund", "Real Madrid",
+                "Barcelona", "Paris Saint-Germain", "Juventus",
+                "AC Milan", "Inter Milan"};
 
-            public class LeithTest {
-                public static void main(String[] args) {
-                    Scanner sc = new Scanner(System.in);
-                    Random rand = new Random();
-                    String input = sc.nextLine();
+        String[] EUTrophys = {"UEFA Champions League", "UEFA Europa League", "UEFA Super Cup", "UEFA Conference League"};
 
-                    Pattern Mercedes = Pattern.compile("(Mercedes|Benz)\\.{6}");
-                    Matcher mercedes = Mercedes.matcher(input);
+        String[] LeagueTitles = {"Premier League", "La Liga", "Bundesliga", "Serie A", "Ligue 1"};
 
-                    if (mercedes.find()) {
-                        System.out.println(mercedes.group());
-                    }
-                }
-            }
-        }
+        String[] DomesticCups = {"FA Cup","Copa del Rey", "DFB-Pokal", "Coppa Italia", "Coupe de France",};
+
+        String[] NTrophys = {"FIFA World Cup", "UEFA Euro", "UEFA Nations League"};
+
+        String BallonDor = "Ballon d'Or";
+
+        int Goals = rand.nextInt(1000);
+        String[] Position = {"Goalkeeper","Centre Back", "Wing Back", "Defensive Midfielder", "Central Midfielder", "Attacking Midfielder", "Winger", "Striker"};
+
+
+        String[] NT = {"Germany", "France", "Italy", "Spain", "England", "Portugal",
+                "Netherlands", "Belgium","Croatia",
+                "Denmark", "Sweden", "Switzerland",
+                "Poland", "Austria", "Turkey", };
     }
-}
+
+
