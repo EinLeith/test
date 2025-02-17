2654b87bb2bfdc5bb2ec36cf9fe5fda94121970c
leith.al-shalbi <leith.alshalbi@analytic-company.com>
2025-02-17 14:55:40 +0100

    added Barca or Real input option

--- azadTest.java
+++ azadTest.java
@@ -1,6 +1,23 @@
-public class azadTest {
-    public static void main(String[] args) {
-        System.out.print("hallo laith");
-        System.out.print("real oder barca"); }
+import java.util.Scanner;
+import java.util.regex.*;
 
+public class azadTest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("hallo laith");
+        System.out.print("real oder barca");
+        Pattern realMadrid = Pattern.compile("(real|Real|Madrid|madrid)");
+        Pattern barca = Pattern.compile("(barca|Barca|Barcelona|barcelona)");
+
+        String userInput = input.nextLine();
+        Matcher realMadridMatcher = realMadrid.matcher(userInput);
+        Matcher barcaMatcher = barca.matcher(userInput);
+
+        if (realMadridMatcher.find()) {
+            System.out.println("Hala Madrid");
+        } else if (barcaMatcher.find()) {
+            System.out.println("Visca Barca");
+        }
+    }
 }
