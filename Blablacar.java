7527dc81384ad89d4f66c22fd0d9711dd478cd08
azad.barlak <azad.barlak@analytic-company.com>
2025-02-18 16:40:48 +0100

    ich habe zwei optionen reingeschrieben

---
+++ Blablacar.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Blablacar {
+
+    public static void main(String[] args) {
+        System.out.println("bla ");
+        System.out.println("bla bla");
+        System.out.println("hi");
+        System.out.print("bla bla bla  ");
+        System.out.print("bla bla bla  ");
+        System.out.println(72727);
+        System.out.println(7+2+4);
+        System.out.println("ja man");
+        System.out.println("was geht");//Heute habe ich einen fetten döner gegessen der war krass lecker
+        String name = "BlaBlaCar Hallo Hallo hi hi";
+        System.out.println(name);
+        int mehmet= 12+34+1234;
+        System.out.println(mehmet);
+    }
+}
---
+++ delivery.java
@@ -0,0 +1,5 @@
+public class delivery {
+    public static void main(String[] args) {
+       float fee = 2.50;
+    }
+}
