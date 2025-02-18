5012e3f63cce84361f79edc961027e5ed264637f
azad.barlak <azad.barlak@analytic-company.com>
2025-02-18 16:48:58 +0100

    ich habe zwei optionen reingeschrieben

--- delivery.java
+++ delivery.java
@@ -1,7 +1,10 @@
 public class delivery {
     public static void main(String[] args) {
 
-        float fee = 2.50f; // Lieferkosten 2,50eur
+        float fee = 2.50f; // Lieferkosten 2,50euro
+        float burger = 3.00f;
+        float fanta = 4.00f;
+        float chickenburger = 5.00f;
 
 
     }
